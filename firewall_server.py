3f65616d15d8a3a3805e13badb31d30360abbe5c
s3860867-Yingyinig-Guo <guo1337993624@126.com>
2022-11-16 19:05:36 +1100

    complete

--- firewall_server.py
+++ firewall_server.py
@@ -9,12 +9,35 @@ port = 8000
 #########
 # Handle the response here 
 def block_request(self):
-    print("Blocking request")
+    self.send_error(403, "Request blocked due to firewall")
 
 def handle_request(self):
+    # List of bad headers from the proof of concept payload
+    bad_headers = {
+        "suffix": "%>//",
+        "c1": "Runtime",
+        "c2": "<%",
+        "DNT": "1",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    bad_header_keys = bad_headers.keys()
+
+    # If a request is on the Spring Framework path
+    if self.path == "/tomcatwar.jsp":
+        # Iterate through bad headers
+        for bad_header_key in bad_header_keys:
+            # If we find a bad header that matches the malicious payload
+            if bad_header_key in self.headers and self.headers[bad_header_key] == bad_headers[bad_header_key]:
+                # Block request and throw 403 error
+                return block_request(self)
+
+    # Return successful response
     self.send_response(200)
     self.send_header("content-type", "application/json")
     self.end_headers()
+
+    self.wfile.write({ "success": True })
 
 #########
 
